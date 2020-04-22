df7d74ee1ce88ca56ff05fe8101c71842430a784
Joe Panzik <jepanzik@gmail.com>
2020-04-22 17:26:13 -0400

    Created IncHistogram_SynthData.py, created description, and set up user inputs

---
+++ InclinationHistogram/Scripts/IncHistogram_SynthData.py
@@ -0,0 +1,11 @@
+#This script is designed to:
+#   1) Create a synthetic inclination array based on geographic coordinates (Lat & Long) and input geomagnetic field geometry (G2 & G3)
+#   2) Create a histogram of the inclination values
+
+# Input values that can be changes: G2 (in %), G3 (in %), Inclination binning size (in degrees)
+
+G2=0;
+G3=0;
+
+IncBin=10;
+
